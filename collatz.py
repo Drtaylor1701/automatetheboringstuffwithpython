193c9dbe336d805daacfb2cef308fb2edf8a7e06
Gillian Weisgram <trekker35@gmail.com>
2019-03-12 09:46:40 -0700

    tries to put collatz in a loop.

--- collatz.py
+++ collatz.py
@@ -2,15 +2,20 @@
 
 #collatz function
 def collatz(number):
+    number = int(number)
     #if the number is even
     if number % 2 == 0:
         #divide number by 2
         number = number // 2
     #if number is odd
-    else:
+    elif number % 2 != 0:
         #multiply number by 3 and add 1
         number = number * 3 + 1
     return number
 
 i = input("Please enter an integer: ")
-print(collatz())
+
+answer = 0
+
+while answer != 0:
+    print(collatz(i))
