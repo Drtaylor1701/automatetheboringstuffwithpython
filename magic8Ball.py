801a35504ffeb97e106267aea6a37d89d7e77966
Gillian Weisgram <trekker35@gmail.com>
2019-11-03 19:09:08 -0800

    finished chapter 3

--- magic8Ball.py
+++ magic8Ball.py
@@ -1,7 +1,26 @@
 #python imports the random module
 import random
 
-messages = ['It is decidedly so', 'Yes definitely', 'Reply hazy try again', 'Ask again later', 'Concentrate and ask again', 'My reply is no', 'Outlook not so good', 'Very doubtful']
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'Outlook not good'
+    elif answerNumber == 8:
+        return 'Very doubtful'
 
-#defines getAnswer function
-print(messages[random.randint(0, len(messages) - 1)])
+#r = random.randint(1, 9)
+#fortune = getAnswer(r)
+#print(fortune)
+
+print(getAnswer(random.randint(1, 9)))
