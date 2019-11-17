2a26169fb2f64d2aa4b247d572ca94bfa7acaf35
Gillian Weisgram <trekker35@gmail.com>
2019-11-16 19:13:57 -0800

    character count started

--- birthdays.py
+++ birthdays.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python
-birthdays = {'Alice': 'Apr 1',
-            'Bob': 'Dec 12',
-            'Carol': 'Mar 4'}
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
 
 while True:
     print('Enter a name: (blank to quit)')
@@ -9,11 +6,11 @@ while True:
     if name == '':
         break
 
-if name in birthdays:
-    print(birthdays[name] + ' is the birthday of ' + name)
-else:
-    print('I do not have birthday information for ' + name)
-    print('What is their birthday?')
-    bday = input()
-    birthdays[name] = birthday
-    print('Birthday database updated.')
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
