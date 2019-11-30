575eb621bab681025c68c2afd54b3b0688ec13a8
Gillian Weisgram <trekker35@gmail.com>
2019-11-30 11:10:27 -0800

    beginning passwordLocker

---
+++ Chapter Projects/Chapter 6/passwordLocker.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+#an attempt at a basic password manager
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
