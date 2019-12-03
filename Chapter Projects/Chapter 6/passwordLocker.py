1d3378d88c2e50e9dd097dfe51fdec7446177b76
Gillian Weisgram <trekker35@gmail.com>
2019-12-03 07:18:14 -0800

    begin chapter project

--- Chapter Projects/Chapter 6/passwordLocker.py
+++ Chapter Projects/Chapter 6/passwordLocker.py
@@ -5,3 +5,18 @@
 PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
              'luggage': '12345'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1] #first command line arg is the account name
+
+sys.argv = ['pw.py', ]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('there is no account named ' + account)
