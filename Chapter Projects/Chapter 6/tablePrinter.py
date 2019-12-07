b3697c8b80fae30495f17e604e23b9a14bfdc662
Gillian Weisgram <trekker35@gmail.com>
2019-12-07 13:32:55 -0800

    more work on table printer

--- Chapter Projects/Chapter 6/tablePrinter.py
+++ Chapter Projects/Chapter 6/tablePrinter.py
@@ -1,1 +1,30 @@
 #! /usr/bin/env python3
+
+def printTable(tableData):
+    colWidths = [0] * len(tableData)
+    print(colWidths)
+
+    listIndex = 0
+    for list in tableData:
+        colWidths[listIndex] = max(len(x) for x in tableData[listIndex])
+        listIndex += 1
+
+    print(colWidths)
+
+    outputTable = []
+    for entry in tableData:
+        for item in entry:
+            outputTable.append(item)
+    
+    return outputTable
+    padding = " "
+    index = 0
+    for list in tableData:
+        print(list[0]) + (padding * (maxlen(list[index])), end='')
+
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+print(printTable(tableData))
