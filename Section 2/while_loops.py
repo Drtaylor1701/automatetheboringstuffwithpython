f3028b046b8f5f52b3aaf6c06429f27de67a3801
drtaylor1701 <trekker35@gmail.com>
2021-12-23 12:54:52 -0800

    finished while loops

--- Section 2/while_loops.py
+++ Section 2/while_loops.py
@@ -6,3 +6,23 @@ while SPAM < 5:
     print("Hello world!")
     SPAM = SPAM + 1
 
+NAME = ''
+while NAME != 'your name':
+    print('Please type your name.')
+    NAME = input()
+print("Thank you!")
+
+'''same as above but with break statement'''
+FIRSTNAME = ''
+while True:
+    print('Please type your name.')
+    if FIRSTNAME == 'your name':
+        break
+print("Thank you!")
+
+SPAM = 0
+while SPAM < 5:
+    SPAM = SPAM + 1
+    if SPAM == 3:
+        continue
+    print('spam is ' + str(SPAM))
