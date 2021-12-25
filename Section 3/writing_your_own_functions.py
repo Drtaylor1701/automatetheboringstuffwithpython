5acd15639b8fd9f91528c5dddadd5ebe698f51cf
drtaylor1701 <trekker35@gmail.com>
2021-12-24 16:54:02 -0800

    working on Section 3

--- Section 3/writing_your_own_functions.py
+++ Section 3/writing_your_own_functions.py
@@ -11,3 +11,25 @@ def hello():
 hello()
 hello()
 hello()
+
+def howdy(name):
+    '''example of writing a function with an argument/parameter'''
+    print('Hello ' + name)
+
+howdy('Alice')
+howdy('Bob')
+
+
+def plus_one(number):
+    '''using a return statement'''
+    return number + 1
+
+NEWNUMBER = plus_one(5)
+print(NEWNUMBER)
+
+'''prints hello world on one line'''
+print('hello', end='')
+print(' world')
+
+#changes separation between words
+print('cat', 'dog', 'mouse', sep='ABC')
